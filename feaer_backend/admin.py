4af941f1e404fc5a6d08899cd5f49824038a940c
Viet <levietviet44266@gmail.com>
2023-04-20 08:21:53 +0700

    tiep tuc setup source

--- feaer_backend/admin.py
+++ feaer_backend/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import Departments,Employees,Product,Category,Collection,Tag,Sex,Discount
+from bson import ObjectId
 
 # Register your models here.
+
+admin.site.register(Departments)
+admin.site.register(Category)
+admin.site.register(Collection)
+admin.site.register(Tag)
+admin.site.register(Sex)
+admin.site.register(Discount)
+
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        if request.POST:
+            # remember old state
+            _mutable = request.POST._mutable
+            # set to mutable
+            request.POST._mutable = True
+            # # сhange the values you want
+            request.POST['Department'] = ObjectId(request.POST['Department'])
+            print('eyeyey ',request.POST['Department'])
+            # # set mutable flag back
+            request.POST._mutable = _mutable
+        return super().get_form(request, obj=obj, **kwargs)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        if request.POST:
+            # remember old state
+            category = request.POST.getlist('Category')
+            print('---___+++ category',category)
+            print('---___+++ category',category)
+            categoryObject = Category.objects.filter(pk__in=category)
+            for cate in categoryObject:
+                # cate._id = ObjectId() 
+                print('----------- cate',cate)
+            _mutable = request.POST._mutable
+            # set to mutable
+            request.POST._mutable = True
+            # # сhange the values you want
+            request.POST['Sex'] = ObjectId(request.POST['Sex'])
+            request.POST['Discount'] = ObjectId(request.POST['Discount'])
+            request.POST['Category'] = [ObjectId(request.POST['Category'])]
+            # request.POST['Collection'] = GenerateObjectIdFromFields(request.POST['Collection'])
+            # request.POST['Tag'] = GenerateObjectIdFromFields(request.POST['Tag'])
+            print('++ ',request.POST)
+            # # set mutable flag back
+            request.POST._mutable = _mutable
+        return super().get_form(request, obj=obj, **kwargs)
+
+def GenerateObjectIdFromFields (fields):
+    # return list(map(lambda x: ObjectId(x),fields))
+    return ObjectId(fields)
--- feaer_backend/models.py
+++ feaer_backend/models.py
@@ -1,13 +1,90 @@
-from django.db import models
+from djongo import models
+from django import forms
+from django.db import models as modelDjango
+import uuid
+
+# from bson import ObjectId
+
+# class DjongoModelChoiceField(forms.ModelChoiceField):
+#     def to_python(self, value):
+#         return self.queryset.get(_id=ObjectId(value))
 
 # Create your models here.
 class Departments(models.Model):
-    DepartmentId = models.AutoField(primary_key=True)
+    _id = models.ObjectIdField()
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     DepartmentName = models.CharField(max_length=250)
+    def __str__(self):
+        return self.DepartmentName
 
 class Employees(models.Model):
-    EmployeeId = models.AutoField(primary_key=True)
+    _id = models.ObjectIdField()
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     EmployeetName = models.CharField(max_length=250)
-    Department = models.CharField(max_length=250)
+    Department = models.ForeignKey('Departments',on_delete=models.CASCADE,db_column='departments')
     DateOfJoining = models.DateField()
     PhotoFileName = models.CharField(max_length=250)
+    def __str__(self):
+        return self.EmployeetName
+
+#CATEGORY 
+class Category(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    def __str__(self):
+        return self.Name
+
+#Collection 
+class Collection(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    Title = models.CharField(max_length=250)
+    Image = models.TextField()
+    Desc = models.TextField()
+    def __str__(self):
+        return self.Name
+
+#Discount 
+class Discount(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    KindOfDiscount = models.CharField(max_length=250)
+    Value = models.IntegerField()
+    def __str__(self):
+        return self.Name
+
+#Sex 
+class Sex(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    def __str__(self):
+        return self.Name
+
+#Tag 
+class Tag(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    def __str__(self):
+        return self.Name
+
+#Product 
+class Product(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=250)
+    Desc = models.TextField()
+    Price = models.IntegerField()
+    FinalPrice = models.IntegerField()
+    Sex = models.ForeignKey('Sex',on_delete=models.CASCADE)
+    Category = models.ManyToManyField(Category)
+    # Category = models.ArrayField(model_container=Category)
+    # Category = models.ArrayField(model_container=Category)
+    # Collection = models.ManyToManyField(Collection)
+    SizeAndStock = models.JSONField(blank=True)
+    Image=models.JSONField(blank=True)
+    ImageDetail=models.JSONField(blank=True)
+    Discount=models.ForeignKey('Discount',on_delete=models.CASCADE)
+    TotalSold=models.IntegerField()
+    # Tag = models.ArrayField(model_container=Tag)
+    # Tag = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.Name
